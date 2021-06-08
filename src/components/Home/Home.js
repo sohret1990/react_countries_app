1bcb672bfc95f7a773cc1ca266c427733d272276
Shohrat Suleymanov <sohret.suleymanov@gmail.com>
2021-06-08 17:57:17 +0400

    ccc

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, View, Text, Button, Image, TextInput } from 'react-native';
+import { ScrollView, View, Text, Button, Image, TextInput, TouchableOpacity } from 'react-native';
 import axios from 'axios'
 export default function Home() {
 
@@ -22,23 +22,23 @@ export default function Home() {
 
     return (
 
-        <View>
+        <View style={{flex:1, margin: 8}}>
 
-            <View style={{ borderWidth: 1, borderColor: 'blue', flex: 1, flexDirection: 'row', borderRadius: 8, marginBottom: 10 }}>
+            <View style={{ borderWidth: 1, borderColor: 'blue', flexDirection: 'row', borderRadius: 8, marginBottom: 10 }}>
                 <TextInput style={{ flex: 1, padding: 8 }} onChange={onSearchQueryValueChange} placeholder="Ölkənin adını daxil edin..." />
-                <Button onPress={{}} title="Axtar"></Button>
+                {/* <Button onPress={{}} title="Axtar"></Button> */}
             </View>
-            <View style={{ borderWidth: 1, borderColor: 'lightgray', flex: 1, alignItems: 'center', justifyContent: 'center', borderRadius: 8, marginBottom: 10, backgroundColor: 'blue', padding: 10 }}>
+            <View style={{ borderWidth: 1, borderColor: 'lightgray', alignItems: 'center', justifyContent: 'center', borderRadius: 8, marginBottom: 10, backgroundColor: 'blue', padding: 10 }}>
                 <Text style={{ fontSize: 20, color: '#ffffff' }}>
                     Ölkə sayı: {countries.length}
                 </Text>
             </View>
-            <ScrollView style={{ flex: 1, margin: 10 }}>
+            <ScrollView>
                 {
                     countries.map(country => {
                         return (
 
-                            <View key={'ContainerView' + country.flag} style={{ flex: 1, flexDirection: 'row', marginBottom: 10, borderWidth: 1, borderRadius: 8, borderColor: 'lightgray', padding: 10 }}>
+                            <TouchableOpacity key={'ContainerView' + country.flag} style={{ flex: 1, flexDirection: 'row', marginBottom: 10, borderWidth: 1, borderRadius: 8, backgroundColor: '#ddeefd', borderColor: 'lightblue', padding: 10 }}>
 
                                 <View>
                                     <Image source={country.flag} style={{ width: 40, height: 30, margin: 5 }} />
@@ -52,7 +52,7 @@ export default function Home() {
                                         {country.capital} ({country.currencies[0].symbol})
                                     </Text>
                                 </View>
-                            </View>
+                            </TouchableOpacity>
                         )
                     })
                 }
